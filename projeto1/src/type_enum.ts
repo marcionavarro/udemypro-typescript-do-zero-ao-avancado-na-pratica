20df188f17f5ad6859d3094342b9d7dd3c3d1804
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-01-28 19:34:36 -0300

    secao-3 [Trabalhando com Tipos]: enum, null e undefined, unknown, assertions

---
+++ projeto1/src/type_enum.ts
@@ -0,0 +1,16 @@
+// Type enum
+
+enum DesignColors {
+    white = "#FFFFFF",
+    black = "#000000"
+}
+
+// console.log(DesignColors.white)
+
+enum StatusPermission {
+    ADMIN,
+    USER,
+    SUPPORT
+}
+
+console.log(StatusPermission.SUPPORT)
